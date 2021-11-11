4c4d50bc44df4932ade3f4f24bc0826908623325
joshua-presley <58670600+joshua-presley@users.noreply.github.com>
2021-11-11 09:51:25 -0500

    fill in algorithm

--- lib/helpers.h
+++ lib/helpers.h
@@ -1,3 +1,6 @@
+#include<iostream>
+using namespace std;
+
 void swap(int &i, int &j){
     int tmp = i;
     i = j;
@@ -5,6 +8,16 @@ void swap(int &i, int &j){
 }
 
 size_t GetArraySize(int arr[]){
+    cout << "Getting array size..." << endl;
     size_t size = sizeof(arr) / sizeof(arr[0]);
+    cout << size << ' ';
+    cout << endl;
     return size;
+}
+
+void PrintArray(int arr[]){
+    size_t size = GetArraySize(arr);
+    for(int i = 0; i<size; i++){
+        cout << arr[i] << ' ';
+    }
 }
