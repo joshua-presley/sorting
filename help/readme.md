66352a3e557d4b6f29481cb14c689042785093ae
joshua-presley <58670600+joshua-presley@users.noreply.github.com>
2021-11-26 16:14:50 -0500

    get sorting to move

--- help/readme.md
+++ help/readme.md
@@ -1,5 +1,5 @@
 Compiling GLUT
 
-g++ -c -o test.o test.c -I"C:\Program Files\Common Files\MinGW\freeglut\include"
+g++ -c -o test.o test.c -I"C:\Program Files\Common Files\MinGW\freeglut\include" -L".\lib"
   
 g++ -o test.exe test.o -L"C:\Program Files\Common Files\MinGW\freeglut\lib\x64" -L".\lib" -lfreeglut -lopengl32 -Wl,--subsystem,windows
--- lib/helpers.h
+++ lib/helpers.h
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include<gl\glut.h>
 
 void Swap(int &i, int &j){
     int tmp = i;
@@ -7,7 +8,42 @@ void Swap(int &i, int &j){
     j = tmp;
 }
 
+void DrawArray(int arr[], size_t size)
+{
 
+    Sleep(250);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    float width = 2.0f/size;
+    float x1 = -1.0f;
+    float x2 = x1 + width;
+
+    for(int i = 0; i < size; i++){
+        float x, y, z;
+        x = 1.0f/(i+0.1);
+        y = i/size;
+        z = size/size*i;
+        glColor3f(x, y, z);
+
+        float y1 = -1.0f;
+        float y2 = (1.0f/arr[i]);
+        glBegin(GL_POLYGON);
+
+
+            glVertex2f(x1, y1);
+            glVertex2f(x2, y1);
+            glVertex2f(x2, y2);
+            glVertex2f(x1, y2);
+        
+        
+        glEnd();
+
+        x1 += width;
+        x2 += width;
+    }
+    
+    glFlush();
+}
 
 void PrintArray(int arr[], size_t size){
     for(int i = 0; i<size; i++){
