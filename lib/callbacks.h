0a7073270a45b58bb08f03803aa2d0a94c626805
joshua-presley <58670600+joshua-presley@users.noreply.github.com>
2021-11-26 13:24:17 -0500

    loop for lines to get drawn

--- lib/callbacks.h
+++ lib/callbacks.h
@@ -18,17 +18,29 @@ void keyboard(unsigned char key, int x, int y)
 /* Display callback function */
 void display(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    int arr[3] = {5, 1, 3};
+    /* Display bars that represent the array */
+    glColor3f(1.0f, 0.0f, 0.0f);
+    int position = 0;
 
-  /* Display a red square */
-  glColor3f(1.0f, 0.0f, 0.0f);
+    for(int i = 0; i < 3; i++){
+        glBegin(GL_POLYGON);
+            //
+            float a = 0.5;
+            float b = 0.3;
+            float x1 = i - a;
+            float x2 = i - b;
+            float y1 = 0.0;
+            float y2 = 1.0;
 
-  glBegin(GL_POLYGON);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f( 0.5f, -0.5f);
-    glVertex2f( 0.5f,  0.5f);
-    glVertex2f(-0.5f,  0.5f);
-  glEnd();
+            glVertex2f(x1, y1);
+            glVertex2f(x2, y1);
+            glVertex2f(x2, y2);
+            glVertex2f(x1, y2);
+        glEnd();
+    }
 
-  glFlush();
+
+    glFlush();
 }
