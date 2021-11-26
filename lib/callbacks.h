44a064f16d51e5cff3b91a6806c41fd8352398c0
joshua-presley <58670600+joshua-presley@users.noreply.github.com>
2021-11-26 13:47:22 -0500

    messing around with line sizes

--- lib/callbacks.h
+++ lib/callbacks.h
@@ -21,24 +21,31 @@ void display(void)
     glClear(GL_COLOR_BUFFER_BIT);
     int arr[3] = {5, 1, 3};
     /* Display bars that represent the array */
+
     glColor3f(1.0f, 0.0f, 0.0f);
-    int position = 0;
+
+    float x1 = -1.0f;
+    float x2 = -0.8f;
 
     for(int i = 0; i < 3; i++){
+
+
+
+        float y1 = 0.0f;
+        float y2 = (1/arr[i]);
         glBegin(GL_POLYGON);
-            //
-            float a = 0.5;
-            float b = 0.3;
-            float x1 = (i - a)/3;
-            float x2 = (i - b)/3;
-            float y1 = 0.0;
-            float y2 = arr[i]/5;
+
 
             glVertex2f(x1, y1);
             glVertex2f(x2, y1);
             glVertex2f(x2, y2);
             glVertex2f(x1, y2);
+        
+        
         glEnd();
+
+        x1 += 0.5f;
+        x2 += 0.5f;
     }
 
 
