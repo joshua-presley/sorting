eac3a055984f1a69224de1fb674b65814ef559f2
joshua-presley <58670600+joshua-presley@users.noreply.github.com>
2021-11-26 13:49:52 -0500

    get colours different too

--- lib/callbacks.h
+++ lib/callbacks.h
@@ -22,17 +22,16 @@ void display(void)
     int arr[3] = {5, 1, 3};
     /* Display bars that represent the array */
 
-    glColor3f(1.0f, 0.0f, 0.0f);
 
     float x1 = -1.0f;
     float x2 = -0.8f;
 
     for(int i = 0; i < 3; i++){
 
-
+        glColor3f(1.0f/arr[i], 0.0f, 0.0f);
 
         float y1 = 0.0f;
-        float y2 = (1/arr[i]);
+        float y2 = (1.0f/arr[i]);
         glBegin(GL_POLYGON);
 
 
